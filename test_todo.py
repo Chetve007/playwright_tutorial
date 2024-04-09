fa7cb9bf1aeef8140a2830b8f03e983dcf6112de
chetve007 <chetve007@mail.ru>
2024-04-09 23:53:45 +0300

    add test with diff locators

--- test_todo.py
+++ test_todo.py
@@ -1,8 +1,16 @@
 
 
-def test_add_todo(browser) -> None:
+def test_add_todo(browser):
     browser.goto("https://demo.playwright.dev/todomvc/#/")
     browser.get_by_placeholder("What needs to be done?").click()
     browser.get_by_placeholder("What needs to be done?").fill("Создать первый сценарий playwright")
     browser.get_by_placeholder("What needs to be done?").press("Enter")
     browser.get_by_role("link", name="Completed").click()
+
+
+def test_add_todo_with_locators(browser):
+    browser.goto("https://demo.playwright.dev/todomvc/#/")
+    browser.locator("#new-todo").click()
+    browser.locator("//*[@id='new-todo']").click()
+    # browser.locator("css=.first-class.another-class").click()
+    # browser.locator("xpath=//div[contains(@class, 'first-class') and contains(@class, 'another-class')]").click()
