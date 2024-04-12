053f79e0ecc618c35f22584030ae23891660e1d6
chetve007 <chetve007@mail.ru>
2024-04-12 23:16:16 +0300

    add test locators diff vars

--- test_locators.py
+++ test_locators.py
@@ -1,3 +1,4 @@
+import pytest
 
 
 def test_loc(browser):
@@ -6,3 +7,65 @@ def test_loc(browser):
     browser.get_by_title("username").fill("Anton")
     browser.get_by_placeholder('password').fill("secret")
     browser.get_by_role('checkbox').click()
+
+
+def test_or(browser):
+    """locator.or_ используется для поиска элемента, который соответствует хотя бы одному из заданных селекторов."""
+    selector = browser.locator("input").or_(browser.locator("text"))
+    selector.fill("Hello Stepik")
+
+
+def test_locator_and(browser):
+    """locator.and_ используется для поиска элемента, который соответствует всем заданным селекторам."""
+    browser.goto("https://zimaev.github.io/locatorand/")
+    selector = browser.get_by_role("button", name="Sing up").and_(browser.get_by_title("Sing up today"))
+    selector.click()
+
+
+def test_locator_chain(browser):
+    browser.goto("https://zimaev.github.io/navbar/")
+    browser.locator("#navbarNavDropdown >> li:has-text('Company')").click()
+    # or
+    # nav_bar = browser.locator('div#navbarNavDropdown')
+    # nav_bar.locator("li:has-text('Company')").click()
+
+
+def test_locator_filter(browser):
+    """ Функция filter() принимает несколько аргументов:
+        - has
+        - has_not
+        - has_text
+        - has_not_text
+    """
+    browser.goto("https://zimaev.github.io/filter/")
+    row_locator = browser.locator("tr")
+    row_locator.filter(has_not=browser.get_by_role("button")).count()
+    row_locator.filter(has_not_text="helicopter")
+
+    # or можно комбинировать, пример:
+    # row_locator = browser.locator("tr")
+    # (row_locator
+    #  .filter(has_text="text in column 1")
+    #  .filter(has=browser.get_by_role("button", name="column 2 button"))
+    #  .click())
+
+    # количество элементов
+    # row_locator = browser.locator("tr")
+    # total = row_locator.filter(has_not=browser.get_by_role("button")).count()
+
+    # browser.locator("li").filter(has_text='Company').click()
+    # browser.locator('li').filter(has=browser.locator('.dropdown-toggle')).click()
+
+
+@pytest.mark.parametrize('how', ('all', 'count'))
+def test_locator_elements(browser, how):
+    """Начиная с версии playwright 1.29 появился специализированный метод locator.all()
+    для перебора всех совпадающих элементов.
+    """
+    browser.goto('https://zimaev.github.io/checks-radios/')
+    checkboxes = browser.locator("input")
+    match how:
+        case 'all':
+            [checkbox.check() for checkbox in checkboxes.all()]
+        case 'count':
+            [checkboxes.nth(ind).click() for ind in range(checkboxes.count())]
